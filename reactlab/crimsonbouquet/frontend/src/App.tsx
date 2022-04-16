16e483c891aabffa8d98677eb61fc787702653d7
Brendan Franz <brendanfranz@dhcp-10-250-97-136.harvard.edu>
2022-04-16 01:58:11 -0400

    react

--- reactlab/crimsonbouquet/frontend/src/App.tsx
+++ reactlab/crimsonbouquet/frontend/src/App.tsx
@@ -4,21 +4,74 @@ import logo from './logo.svg';
 import './App.css';
 import { gql, useQuery } from '@apollo/client'
 
+const GET_ALL_CONTENT = gql`
+  query GetAllContent {
+    allContent {
+      title
+      slug
+    }
+  }
+`
+
+interface ArticleGQL {
+  title: string;
+  slug: string;
+}
+
 const MainPage = function() {
+  const { loading, error, data } = useQuery(GET_ALL_CONTENT);
+
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p>Error :(</p>;
+  
+  console.log(data)
+
+  const Lists = data.allContent.map((a: ArticleGQL) => (
+    <li key={a.slug}><Link to={'article/' + a.slug}>{a.title}</Link></li>
+  ));
+
   return (
     <div className="MainPage">
-      i am the main page now
+      <h1>Articles</h1>
+      <ul>
+        { Lists }
+      </ul>
     </div>
   )
 }
 
+const GET_ARTICLE = gql`
+  query GetArticle($slug: String!) {
+    content(slug: $slug) {
+      title
+      text
+      contributors {
+        firstName
+        lastName
+      }
+    }
+  }
+`
+
 const ArticlePage = function() {
+  const params = useParams()
+  console.log(params);
+  const {loading, error, data} = useQuery(GET_ARTICLE, {variables: params})
+  
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log(data)
+
   return (
     <div className="article">
-      i am a page lol
+      <h1> {data.content.title} </h1>
+      <div className="body" dangerouslySetInnerHTML={{__html: data.content.text}} />
     </div>
   )
 }
+
+
 
 function App() {
   return (
