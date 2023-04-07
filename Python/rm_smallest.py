4405f5fd9fa222fe0b2452192c2096b38a64e957
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2023-04-07 00:10:05 -0400

    Initial Update.

--- Python/rm_smallest.py
+++ Python/rm_smallest.py
@@ -1,12 +1,27 @@
+# take in argument d
 def rm_smallest(d):
-    # Your code here!
-    return 0;
+    # if the dictionary is empty, return the original dictionary
+    if not d:
+        return d
+
+    # find the minimum value in the dictionary
+    min_value = min(d.values())
+
+    # find the key(s) that correspond to the minimum value
+    min_keys = [k for k, v in d.items() if v == min_value]
+
+    # remove the key-value pair(s) from the dictionary
+    for k in min_keys:
+        d.pop(k)
+
+    # return the modified dictionary
+    return d
 
 def test():
     assert 'a' in rm_smallest({'a':1,'b':-10}).keys()
     assert not 'b' in rm_smallest({'a':1,'b':-10}).keys()
     assert not 'a' in rm_smallest({'a':1,'b':5,'c':3}).keys()
-    rm_smallest({})
+    assert rm_smallest({}) == {}
     print("Success!")
 
 if __name__ == "__main__":
