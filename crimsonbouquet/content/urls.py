ecdb8c3b3397a2ee0c8242324751785206351d33
jbejjani2022 <51431082+jbejjani2022@users.noreply.github.com>
2023-04-15 14:32:46 -0400

    django portion done

--- crimsonbouquet/content/urls.py
+++ crimsonbouquet/content/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contrib/<int:contrib_id>/', views.display_contributor, name = "display_contributor"),
+    path('article/<slug:slug>/', views.display_article, name = "display_article")
 ]
